715e855200957ec13cc8ed75e576eaee71819574
darmyn <81786165+darmyn@users.noreply.github.com>
2022-11-16 22:30:22 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -8,27 +8,7 @@ prefab.Position = Vector3.new(0, 5, 0)
 prefab.Anchored = true
 
 local myRecycledInstance = recycledInstance.new(prefab)
-print("test")
-local generatedPerSecond = .03
 
-task.spawn(function()
-	while true do
-		generatedPerSecond = .03
-		print("rate updated: ", generatedPerSecond)
-		task.wait(3)
-		generatedPerSecond = .05
-		print("rate updated: ", generatedPerSecond)
-		task.wait(3)
-		generatedPerSecond = .1
-		print("rate updated: ", generatedPerSecond)
-		task.wait(3)
-		generatedPerSecond = .03
-		print("rate updated: ", generatedPerSecond)
-		task.wait(3)
-		generatedPerSecond = .25
-		print("rate updated: ", generatedPerSecond)
-	end
-end)
 while true do
 	local newInstance = myRecycledInstance:clone()
 	newInstance.Position = prefab.Position + Vector3.new(math.random(1, 5), math.random(1, 5), math.random(1, 5))
